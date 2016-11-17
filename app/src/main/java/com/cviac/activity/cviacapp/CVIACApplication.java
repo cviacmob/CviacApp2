ea8265b72c3cedeec70a8dd8754f6ae888c8f6d9
cviacmob <cviacmobility@gmail.com>
2016-11-17 17:56:53 +0530

    db added

---
+++ app/src/main/java/com/cviac/activity/cviacapp/CVIACApplication.java
@@ -0,0 +1,25 @@
+package com.cviac.activity.cviacapp;
+
+
+import android.app.Application;
+
+import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Configuration;
+import com.cviac.datamodel.cviacapp.ChatMessage;
+
+/**
+ * Created by Cviac on 17/11/2016.
+ */
+
+public class CVIACApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Configuration.Builder configurationBuilder = new Configuration.Builder(this);
+        configurationBuilder.addModelClasses(ChatMessage.class);
+        ActiveAndroid.initialize(configurationBuilder.create());
+    }
+}
+
+
